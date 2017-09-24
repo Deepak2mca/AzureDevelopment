a114fbd3bc720778e17c6e3c9188225aa75e4390
Deepak2mca <sahu.deepak01@gmail.com>
2017-09-24 19:04:56 +0530

    test

---
+++ AugularCRUD/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AugularCRUD.Startup))]
+namespace AugularCRUD
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
